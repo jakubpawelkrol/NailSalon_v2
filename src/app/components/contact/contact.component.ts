acf1b05e961c3573d3134dd089fbacabf348d029
Jakub Krol <redmail@gazeta.pl>
2025-08-09 00:25:03 +0200

    contact page under construction

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  contactForm = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  submitForm() {
+    // TODO: real email functionality
+    console.log('Form submitted:', this.contactForm);
+    alert('Thank you for your message!');
+    this.contactForm = { name: '', email: '', message: '' }; // reset
+  }
 }

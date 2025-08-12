2a67838ff8d0b18c48cd89a4df62f80495afd2a5
Jakub Krol <redmail@gazeta.pl>
2025-08-12 14:48:58 +0200

    scheduler tweaks, icon change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SiteHeaderComponent],
+  imports: [RouterOutlet, SiteHeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })

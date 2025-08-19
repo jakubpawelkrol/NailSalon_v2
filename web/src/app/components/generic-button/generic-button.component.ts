234f201fd6ca5b7ecf8bbdec416700037b751033
Jakub Krol <redmail@gazeta.pl>
2025-08-19 23:00:25 +0200

    button in progress

---
+++ web/src/app/components/generic-button/generic-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-button',
+  imports: [],
+  templateUrl: './generic-button.component.html',
+  styleUrl: './generic-button.component.css'
+})
+export class GenericButtonComponent {
+
+}
